9e6cc3b3b0a2c5c228adab7d348f902e70b8319d
dragos <dragos.ignat30@gmail.com>
2025-05-12 16:06:50 +0000

    CORS

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from app.api.endpoints import router
 from app.database.database import engine
 from app.models.models import Base
 from app.core.config import get_settings
+from fastapi.middleware.cors import CORSMiddleware
 
 # Load settings
 settings = get_settings()
@@ -19,6 +20,20 @@ app = FastAPI(
 
 # Include routers
 app.include_router(router, prefix="/api")
+
+# Include cors
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 if __name__ == "__main__":
     import uvicorn
